ebe18323dd1cab90ba6ebb035b0db3cafb690dba
Godseed Chukwuemeka <godseedchukwuemeka@gmail.com>
2023-03-29 11:12:35 +0100

    An AI application landing page built with react

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-        Blog
+    <div className='gpt3__blog section__padding' id='blog'>
+       <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+       </div>
+       <div className='gpt3__blog-container'>
+          <div className='gpt3__blog-container_groupA'>
+            <Article imgurl={blog01} date='sep 26, 2021' title= 'GPT-3 and open AI is the future. Let us explore how it is?' />
+          </div>
+          <div className='gpt3__blog-container_groupB'>
+            <Article imgurl={blog02} date='sep 26, 2021' title= 'GPT-3 and open AI is the future. Let us explore how it is?' />
+            <Article imgurl={blog03} date='sep 26, 2021' title= 'GPT-3 and open AI is the future. Let us explore how it is?' />
+            <Article imgurl={blog04} date='sep 26, 2021' title= 'GPT-3 and open AI is the future. Let us explore how it is?' />
+            <Article imgurl={blog05} date='sep 26, 2021' title= 'GPT-3 and open AI is the future. Let us explore how it is?' />
+          </div>
+       </div>
     </div>
   )
 }
